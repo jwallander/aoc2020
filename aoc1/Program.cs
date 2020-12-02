d68e94b2b497d362916e67beb1975f10c59c7e5d
Jonas Wallander <jwallander@gmail.com>
2020-12-02 18:52:48 +0100

    first commit

---
+++ aoc1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace aoc1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            FileStream fileStream = new FileStream("data.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    numbers.Add(int.Parse(reader.ReadLine()));
+                }
+                
+            }
+            
+            int answer = 0;
+            int ax = 0;
+            int ay = 0;
+            foreach (var x in numbers)
+            {
+                foreach (var y in numbers)
+                {
+                    if (x + y == 2020)
+                    {
+                       answer = x * y;
+                       ax = x;
+                       ay = y;
+                       break; 
+                    }
+                }
+                if (answer != 0)
+                {
+                    break;
+                }
+            }
+   
+            int answer2 = 0;
+            int ax2 = 0;
+            int ay2 = 0;
+            int az2 = 0;
+            foreach (var x in numbers)
+            {
+                foreach (var y in numbers)
+                {
+                    foreach (var z in numbers)
+                    {
+                        if (x + y + z == 2020)
+                        {
+                            answer2 = x * y * z;
+                            ax2 = x;
+                            ay2 = y;
+                            az2 = z;
+                            break;
+                        }
+                    }
+                    if (answer2 != 0)
+                    {
+                        break;
+                    }
+                }
+                if (answer2 != 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(ax);
+            Console.WriteLine(ay);
+            Console.WriteLine(answer);
+            Console.WriteLine(ax2);
+            Console.WriteLine(ay2);
+            Console.WriteLine(az2);
+            Console.WriteLine(answer2);
+        }
+    }
+}
---
+++ aoc2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc2
+{
+    class Program
+    {
+        private static List<string> pwds;
+
+        static void Main(string[] args)
+        {
+            pwds = new List<string>();
+            FileStream fileStream = new FileStream("data.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    pwds.Add(reader.ReadLine());
+                }
+                
+            }
+            
+
+            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
+        }
+
+        private static int Part1()
+        {
+            int answer = 0;
+
+            foreach (var x in pwds)
+            {
+                var kpos = x.IndexOf(":");
+                var c = x.Substring(kpos-1,1);
+                var dpos = x.IndexOf("-");
+                var llim = int.Parse(x.Substring(0,dpos));
+                var hlim = int.Parse(x.Substring(dpos+1,(kpos-2)-(dpos+1)));
+                var pw = x.Substring(kpos+2);
+                var count = pw.Count(ch => ch == Convert.ToChar(c));  
+                if (count >= llim && count <= hlim)
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+
+        private static int Part2()
+        {
+            int answer = 0;
+
+            foreach (var x in pwds)
+            {
+                var kpos = x.IndexOf(":");
+                var c = x.Substring(kpos-1,1);
+                var dpos = x.IndexOf("-");
+                var index1 = int.Parse(x.Substring(0,dpos))-1;
+                var index2 = int.Parse(x.Substring(dpos+1,(kpos-2)-(dpos+1)))-1;
+                var pw = x.Substring(kpos+2);
+                if (pw.Substring(index1,1) == c ^ pw.Substring(index2,1) == c)
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
