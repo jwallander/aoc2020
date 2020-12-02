fa5f9143c2670078789ba6c85d265ce2f98c456e
Jonas Wallander <jwallander@gmail.com>
2020-12-02 19:20:22 +0100

    aoc3 prep

---
+++ aoc3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc3
+{
+    class Program
+    {
+        private static List<string> pwds;
+
+        static void Main(string[] args)
+        {
+            pwds = new List<string>();
+            FileStream fileStream = new FileStream("data.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    pwds.Add(reader.ReadLine());
+                }
+                
+            }
+            
+
+            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
+        }
+
+        private static int Part1()
+        {
+            int answer = 0;
+
+            return answer;
+        }
+
+        private static int Part2()
+        {
+            int answer = 0;
+
+            return answer;
+        }
+    }
+}
