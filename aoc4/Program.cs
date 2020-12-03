d187368a103031daddb1a8905d131a522edd6632
Jonas Wallander <jonas.wallander@tetrapak.com>
2020-12-03 13:54:12 +0100

    Day 4 prep.

---
+++ aoc4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc4
+{
+    class Program
+    {
+        private static List<string> lines;
+
+        static void Main(string[] args)
+        {
+            lines = new List<string>();
+            FileStream fileStream = new FileStream("data.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    lines.Add(reader.ReadLine());
+                }
+                
+            }
+            
+            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
+        }
+
+        private static int Part1()
+        {
+            int answer = 0;
+
+
+            return answer;
+        }
+
+        private static int Part2()
+        {
+            int answer = 0;
+
+
+            return answer;
+        }
+    }
+}
