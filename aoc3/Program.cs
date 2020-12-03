b18f95961d81da086efa1ad8e28c3c0642df3d4b
Jonas Wallander <jonas.wallander@tetrapak.com>
2020-12-03 13:41:47 +0100

    Day 3 second answer.

--- aoc3/Program.cs
+++ aoc3/Program.cs
@@ -34,23 +34,60 @@ namespace aoc3
             int answer = 0;
             int xpos;
 
-            for (int ypos = 1; ypos < lines.Count; ypos++)
+            for (int ypos = 0; ypos < lines.Count; ypos++)
             {
                 xpos = (ypos*3) % lineLength;
-                if(lines[ypos].Substring(xpos,1) == "#")
-                {
-                    answer++;
-                }
+                answer += TreeCount(xpos, ypos);
             }
 
             return answer;
         }
 
-        private static int Part2()
+        private static Int64 Part2()
         {
-            int answer = 0;
+            var slopes = new List<Tuple<int, int>> 
+            { 
+                new Tuple<int, int>(1, 1), 
+                new Tuple<int, int>(3, 1), 
+                new Tuple<int, int>(5, 1), 
+                new Tuple<int, int>(7, 1), 
+                new Tuple<int, int>(1, 2), 
+            };  
+
+
+            int lineLength = lines?.First().Length ?? 1; 
+
+            Int64 answer = 1;
+            int slopeAnswer = 0;
+            int xpos;
+
+            foreach (var slope in slopes)
+            {
+                for (int ypos = 0; ypos < lines.Count; ypos += slope.Item2)
+				{
+					var slopeRate = ((double) slope.Item1) / ((double) slope.Item2);
+					xpos = (int)((ypos * slopeRate) % lineLength);
+					slopeAnswer += TreeCount(xpos, ypos);
+				}
+
+				answer = answer * slopeAnswer;
+                Console.WriteLine(slopeAnswer);
+                slopeAnswer = 0;
+            }
 
             return answer;
         }
-    }
+
+		private static int TreeCount(int xpos, int ypos)
+		{
+            var count = 0;
+			bool isTree = lines[ypos].Substring(xpos, 1) == "#";
+			if (isTree)
+			{
+				count = 1;
+			}
+
+			return count;
+		}
+	}
 }
